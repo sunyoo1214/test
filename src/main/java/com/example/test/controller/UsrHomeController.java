145676318b5d563a5c641d694d52c3426e6bca2c
sunyoo1214 <sunyoo1214@gmail.com>
2023-04-27 23:03:42 +0900

    count 값 초기화

--- src/main/java/com/example/test/controller/UsrHomeController.java
+++ src/main/java/com/example/test/controller/UsrHomeController.java
@@ -7,31 +7,37 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class UsrHomeController {
 
-    int cnt = 0;
+	int cnt = 0;
+
 	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String showMain() {
 		return "안녕하세요.";
 	}
-	
+
 	@RequestMapping("/usr/home/main2")
 	@ResponseBody
 	public String showMain2() {
 		return "반갑습니다.";
 	}
-	
+
 	@RequestMapping("/usr/home/main3")
 	@ResponseBody
 	public String showMain3() {
 		return "또만나요.";
 	}
-	
+
 	@RequestMapping("/usr/home/main4")
 	@ResponseBody
 	public int showMain4() {
-		
 		cnt++;
-		
 		return cnt;
+	}
+
+	@RequestMapping("/usr/home/main5")
+	@ResponseBody
+	public String showMain5() {
+		cnt = 0;
+		return "count값이 초기화되어 0 입니다";
 	}
 }
