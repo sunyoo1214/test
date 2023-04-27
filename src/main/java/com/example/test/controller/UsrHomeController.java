8d4818ccab8fa1dfe95d94b9ac3995026678bfe5
sunyoo1214 <sunyoo1214@gmail.com>
2023-04-28 01:02:28 +0900

    passenger 추가시 no 증가

--- src/main/java/com/example/test/controller/UsrHomeController.java
+++ src/main/java/com/example/test/controller/UsrHomeController.java
@@ -1,5 +1,8 @@
 package com.example.test.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,13 +12,24 @@ import com.example.test.vo.Passenger;
 @Controller
 public class UsrHomeController {
 
+	private List<Passenger> passengers;
+	private int passengerLastNo;
+	
+	public UsrHomeController() {
+		passengers = new ArrayList<>();
+		passengerLastNo = 0;
+	}
+	
 	@RequestMapping("/usr/passenger/doAdd")
 	@ResponseBody
 	public Passenger doAdd(String name, String id, String gender) {
 
-		int no = 1;
+		int no = passengerLastNo + 1;
 		Passenger passenger = new Passenger(no, name, id, gender);
 
+		passengers.add(passenger);
+		
+		passengerLastNo = no;
 		return passenger;
 	}
 
